5fbbf4c397de6ecb6c832fff3ba2a4a19cbb1401
Gustavo Ferreira <gustavoferreira22@hotmail.com>
2024-08-12 08:58:22 -0500

    Add project files.

---
+++ Api.Users/Controllers/PersonController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Users.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[ApiController]
+    //[Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        private List<Person> GetPersons()
+        {
+            return new List<Person>
+            {
+                new Person { Id = 1, Name = "Juan Pérez", Age = 30 },
+                new Person { Id = 2, Name = "Ana Gómez", Age = 25 },
+                new Person { Id = 3, Name = "Carlos López", Age = 40 }
+            };
+        }
+
+        [HttpGet(Name = "GetPersons")]
+        public ActionResult<Person> Get()
+        {
+            var persons = GetPersons();
+            return Ok(persons);
+
+        }
+    }
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
---
+++ Yarp.Test/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
+
+var proxyConfig = builder.Configuration.GetSection("ReverseProxy");
+builder.Services.AddReverseProxy().LoadFromConfig(proxyConfig);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.UseRateLimiter();
+
+app.MapReverseProxy();
+
+app.Run();
